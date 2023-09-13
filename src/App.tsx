2dc552468042857e30a595e18b1d4c37c99d79de
matheuszwf <matheuszwf@gmail.com>
2023-09-13 14:37:25 -0300

    feat: add app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
+import { Separator } from "./components/ui/separator";
+import { Header } from "./components/header";
+import { Prompt } from "./components/prompt";
+import { UploadForm } from "./components/uploadForm";
+import { ProcessForm } from "./components/ProcessForm";
 
 export function App() {
-
   return (
-    <h1>video.ai</h1>
+    <div className="min-h-screen flex flex-col">
+      <Header className=""/>
+
+      <main className="container flex flex-1 py-4 gap-6">
+        <section className="flex flex-1 flex-col gap-4">
+          <Prompt className="max-h-[734px]"/>
+
+          <p className="text-sm text-muted-foreground">
+            Remember: you can use the <code className="text-primary font-semibold">{'{transcription}'}</code> variable
+            in your prompt to add the content of the selected video's transcription.
+          </p>
+        </section>
+        
+        <aside className="w-[306px] space-y-4">
+          <UploadForm />
+          <Separator />
+          <ProcessForm />
+        </aside>
+      </main>
+    </div>
   )
 }
